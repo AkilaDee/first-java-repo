19d1f0418f0da25da69b9b5190a8a14a7afb47c7
Akila De Silva <54980465+AkilaDee@users.noreply.github.com>
2020-08-03 12:40:38 +0530

    Add files via upload

---
+++ packages/SimpleArithmeticCalculator.java
@@ -0,0 +1,21 @@
+package arithmeticCalculator;
+
+public class SimpleArithmeticCalculator{
+
+    public int add(int num1,int num2){
+        return num1+num2;
+    }
+
+    public int subtract(int num1,int num2){
+        return num1-num2;
+    }
+
+    public int multiplication(int num1,int num2){
+        return num1*num2;
+    }
+
+    public double division(int num1,int num2){
+        return (double)num1/(double)num2;
+    }
+
+}
